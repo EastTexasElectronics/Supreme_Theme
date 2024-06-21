789bd9deb944c3fa75d9929def0e366b512cb762
rmh <Contact@EastTexaselectronics.com>
2024-06-20 21:56:48 -0500

    Navbar Progress
    Navbar is successfully connected.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layout/*.liquid', './sections/*.liquid', './templates/*.liquid', './snippets/*.liquid', './assets/application.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
