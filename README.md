2c8695952fe0a298b4013419b1535cfeca90fa79
rmh <Contact@EastTexaselectronics.com>
2024-06-25 13:07:28 -0500

    went to the dark side

--- README.md
+++
@@ -1,8 +0,0 @@
-
-# ReadMe
-
-Tailwind cmd:
-npx tailwind -i ./src/tailwind.css -o ./assets/application.css --watch
-
-Shopify cmd:
-sudo shopify theme dev
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./layout/*.liquid', './sections/*.liquid', './templates/*.liquid', './snippets/*.liquid', './assets/application.css'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
